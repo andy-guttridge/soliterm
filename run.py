0be1030ee00a3680f5b3bf2f48cd00038c171c30
Andy G <andyguttridge123@gmail.com>
2022-08-03 09:27:01 +0000

    Feature: add options to show instructions or quit from the game

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 import curses
 from curses import wrapper
 import numpy as np
+from sys import exit
 
 
 class TermManager:
@@ -155,7 +156,7 @@ def show_title(term_manager):
 
     # Prompt player to press a key in the bottom window
     # and wait for key press.
-    term_manager.show_msg(0, "Press a key to start")
+    term_manager.show_msg(0, "Press a key when ready")
     term_manager.bottom_win.getkey()
 
 
@@ -238,12 +239,13 @@ def get_move(term_manager):
     """
     # Print example of valid move and prompt player to enter move
     term_manager.show_msg(0, "Move format is column, row, "
-                          "direction, e.g. h10u", "Enter next move> ")
+                          "direction, e.g. h10u", "or enter i for instructions" 
+                          ", or q to quit", " ", "Enter next move> ")
     
     # Echo user input to terminal, get input and turn off echo
     curses.echo()
     player_input =\
-        term_manager.bottom_win.getstr(1, 18, 4).decode(encoding="utf=8")
+        term_manager.bottom_win.getstr(3, 18, 4).decode(encoding="utf=8")
     curses.noecho()
 
     return player_input
@@ -256,10 +258,20 @@ def validate_format(move):
 
     Returns a tuple containing a bool to indicate if the move was
     in a valid format, and the row, column and direction of the move
-    if it was valid.
+    if it was valid. Also returns special values for quit or 
+    instructions.
 
     Row and column are integers, direction is a string.
     """
+
+    # If player enters q for quit return special value
+    if move.lower() == "q":
+        return(True, -1, 0, "0")
+    
+    # If player enters i for instructions return special value
+    if move.lower() == "i":
+        return(True, -2, 0, "0")
+
     # If move is not 3 or 4 characters, it is invalid.
     if len(move) > 4 or len(move) < 3:
         return(False, 0, 0, "0")
@@ -479,17 +491,25 @@ def main(stdscr):
             while not valid_move:
 
                 # Get input from the player and return to start of loop if not
-                # in a valid format
+                # in a valid format. 
                 next_move = get_move(term_manager)
                 formatted_move = validate_format(next_move)
                 if formatted_move[0] is False:
-                    term_manager.show_msg(3, "Invalid format - try again")
+                    term_manager.show_msg(4, "Invalid format - try again")
+                    continue
+                # Check for special cases and exit or display instructions
+                # if appropriate
+                elif formatted_move[1] == -1:
+                    exit("Soliterm exited")
+                elif formatted_move[1] == -2:
+                    show_title(term_manager)
+                    draw_board(game_board, term_manager)
                     continue
                 
                 # Check if the move is valid and return to start of loop if not
                 validated_move = validate_move(formatted_move, game_board)
                 if validated_move["valid"] is False:
-                    term_manager.show_msg(3, "Invalid move - try again")
+                    term_manager.show_msg(4, "Invalid move - try again")
                     continue
 
                 # Extract the coordinates of cell the player is moving from,
@@ -506,7 +526,7 @@ def main(stdscr):
                 game_board.update_stats()
                 draw_board(game_board, term_manager)
 
-                term_manager.show_msg(3, "Great move! Next turn")
+                term_manager.show_msg(4, "Great move! Next turn")
                 
                 # Player has made a valid move so update flag to exit loop
                 valid_move = True
